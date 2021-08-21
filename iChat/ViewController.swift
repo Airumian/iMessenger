227d4e6ed8cb5a88bb15481b7aad411426037e50
Airumian <a5s5r5@mail.ru>
2021-08-21 17:04:10 +0300

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Alexander Airumyan on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
