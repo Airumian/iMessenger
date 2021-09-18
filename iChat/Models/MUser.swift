b4661d78a6f1038305eeb9ff497359e79c32d731
Airumian <a5s5r5@mail.ru>
2021-09-18 18:32:09 +0300

    Lesson 20 (App Users Screen)

---
+++ iChat/Models/MUser.swift
@@ -0,0 +1,22 @@
+//
+//  MUser.swift
+//  iChat
+//
+//  Created by Alexander Airumyan on 18.09.2021.
+//
+
+import UIKit
+
+struct MUser: Hashable, Decodable {
+    var username: String
+    var avatarStringURL: String
+    var id: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: MUser, rhs: MUser) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
